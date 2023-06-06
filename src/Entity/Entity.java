3640c9f3932ed9279fb9f08bf7539d23ece8a021
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-06-06 20:41:44 +0700

    Change x,y,speed attributes from double to int

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,10 +1,11 @@
 package Entity;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public abstract class Entity {
-    public double x, y, speed;
+    public int x, y, speed;
     public BufferedImage run1, run2, run3, run4, run5, run6;
     public BufferedImage down1, down2, down3, down4, down5, down6;
     public BufferedImage up1, up2;
@@ -13,4 +14,5 @@ public abstract class Entity {
     public int spriteCounter = 0;
     public int spriteNum = 1;
     public int jumpCounter = 0;
+    public Rectangle solidArea;
 }
