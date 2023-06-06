aa61f27daf6ed5bb1f6b95725d942d969186d7d1
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-06-06 23:41:23 +0700

    Add obstacle spawner

--- src/tile/Obstacle.java
+++ src/tile/Obstacle.java
@@ -1,10 +1,45 @@
 package tile;
 
+import main.GamePanel;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Obstacle {
     public BufferedImage image;
     public Rectangle solidArea;
     public int x, y, width, height;
+    public GamePanel gp;
+    public Obstacle(GamePanel gp, int x, int y){
+        this.gp = gp;
+        this.x = x;
+        this.y = y;
+        this.width = gp.tileSize;
+        this.height = gp.tileSize;
+
+
+        this.solidArea = new Rectangle();
+        solidArea.x = x;
+        solidArea.y = getSolidAreaY();
+        solidArea.width = gp.tileSize;
+        solidArea.height = (int) (gp.tileSize*0.4);
+
+        getImage();
+        System.out.println("Obstacle Spawned");
+    }
+    public void getImage(){
+        try {
+            this.image = ImageIO.read(getClass().getResourceAsStream("/tiles/spike.png"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public int getSolidAreaX(){
+        return x;
+    }
+    public int getSolidAreaY(){
+        return (int) ( y + (0.6*gp.tileSize));
+    }
 }
--- src/tile/ObstacleManager.java
+++ src/tile/ObstacleManager.java
@@ -7,58 +7,55 @@ import main.GamePanel;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ObstacleManager {
     GamePanel gp;
-    Obstacle[] obstacles;
+    ArrayList<Obstacle> obstacles;
     Player player1, player2;
-    int x, y;
+    int x, y, spawnCounter;
 
     public ObstacleManager(GamePanel gp, Player player1, Player player2){
         this.gp = gp;
         x = gp.tileSize*4;
         y = gp.tileSize*3;
-        obstacles = new Obstacle[1];
-        getObstacleImage();
+        obstacles = new ArrayList<>();
         this.player1 = player1;
         this.player2 = player2;
+        spawnCounter = 0;
     }
-    public void spawnObstacle(){}
     public void update(){
+        spawnCounter++;
         for (Obstacle obs : obstacles) {
             checkCollision(player1, obs);
         }
+        if(spawnCounter%60 == 1){
+            spawnObstacle();
+        }
     }
+    // TODO
+    // Every few seconds, spawn a new obstacle
+    // move every obstacle to the left by the speed
+    // change speed
+    public void spawnObstacle(){
+        obstacles.add(new Obstacle(gp, gp.tileSize*4 + spawnCounter, gp.tileSize*3));
+    }
+
     public void checkCollision(Player player, Obstacle obs){
         int px1 = player.getSolidAreaX() ;
         int px2 = player.getSolidAreaX() + player.solidArea.width;
         int ox1 = obs.x;
-        int ox2 = obs.x + obs.width;
+        int ox2 = obs.x + obs.solidArea.width;
 
         int py1 = player.getSolidAreaY();
         int py2 = player.getSolidAreaY() + player.solidArea.height;
         int oy1 = obs.y;
-        int oy2 = obs.y + player.solidArea.height;
+        int oy2 = obs.y + obs.solidArea.height;
 
 //        System.out.println("PX1: "+px1+ " PX2: "+px2+" OX1: "+ox1+" OX2 "+ox2);
+        // Intersection antara player dan obstacle
         if (px1 < ox2 && px2 > ox1 && py1 < oy2 && py2 > oy1){
             System.out.println("GAME OVER");
-//            System.out.println(px1 < ox2);
-//            System.out.println("Game Over  "+"PX1: "+px1+ " PX2: "+px2+" OX1: "+ox1+" OX2 "+ox2);
-        }
-    }
-    public void getObstacleImage(){
-        try {
-            obstacles[0] = new Obstacle();
-            obstacles[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/spike.png"));
-            obstacles[0].solidArea = new Rectangle();
-            obstacles[0].x = gp.tileSize*4;
-            obstacles[0].y = (int) (gp.tileSize*3 + (0.6*gp.tileSize));
-            obstacles[0].width = gp.tileSize;
-            obstacles[0].height = (int) (gp.tileSize*0.4);
-
-        } catch (IOException e){
-            e.printStackTrace();
         }
     }
 
@@ -66,8 +63,13 @@ public class ObstacleManager {
 //        g2.setColor(Color.white);
 //        g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
 
-        g2.fillRect(obstacles[0].x, obstacles[0].y, obstacles[0].width, obstacles[0].height);
-        g2.drawImage(obstacles[0].image, x, y, gp.tileSize, gp.tileSize, null);
+        // TODO buat loop untuk nampilin setiap elemen di list obstacles
+        for (Obstacle obs: obstacles) {
+            g2.fillRect(obs.solidArea.x, obs.getSolidAreaY(), obs.width, obs.solidArea.height);
+            g2.drawImage(obs.image, obs.x, obs.y, gp.tileSize, gp.tileSize, null);
+        }
+//        g2.fillRect(obstacles[0].x, obstacles[0].y, obstacles[0].width, obstacles[0].height);
+//        g2.drawImage(obstacles[0].image, x, y, gp.tileSize, gp.tileSize, null);
 
     }
 }
